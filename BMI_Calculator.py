d270bd2e06ee426099d56c36a7538cc06b28354b
Michel Brassart <mikebrassart@gmail.com>
2024-04-21 23:23:04 -0500

    Updated BMI to format and only print 2 decimal

--- BMI_Calculator.py
+++ BMI_Calculator.py
@@ -1,14 +1,15 @@
-height = float(input())  # in meters e.g., 1.55
-weight = int(input())  # in kilograms e.g., 72
+height = float(input("What is your height in meters e.g. 1.82:  "))  # in meters e.g., 1.55
+weight = int(input("What is your weight in kilograms e.g. 72:  "))  # in kilograms e.g., 72
 # Your code below this line 👇
 bmi = weight / (height * height)
+formatted_bmi = f"{bmi:.2f}"
 if bmi < 18.5:
-  print(f"Your BMI is {bmi}, you are underweight.")
+  print(f"Your BMI is {formatted_bmi}, you are underweight.")
 elif bmi < 25:
-  print(f"Your BMI is {bmi}, you have a normal weight.")
+  print(f"Your BMI is {formatted_bmi}, you have a normal weight.")
 elif bmi < 30:
-  print(f"Your BMI is {bmi}, you are slightly overweight.")
+  print(f"Your BMI is {formatted_bmi}, you are slightly overweight.")
 elif bmi < 35:
-  print(f"Your BMI is {bmi}, you are obese.")
+  print(f"Your BMI is {formatted_bmi}, you are obese.")
 else:
-  print(f"Your BMI is {bmi}, you are clinically obese.")
+  print(f"Your BMI is {formatted_bmi}, you are clinically obese.")
