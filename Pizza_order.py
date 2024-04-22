9365d3b4698a343f360091990bdd8af6018ed648
Michel Brassart <mikebrassart@gmail.com>
2024-04-22 02:19:44 -0500

    Initial Commit

---
+++ Pizza_order.py
@@ -0,0 +1,24 @@
+
+size = input('What size pizza do you want? "S", "M", or "L": ').lower()
+add_pepperoni = input('Do you want pepperoni? "Y" or "N": ').lower()
+extra_cheese = input('Do you want extra cheese? "Y" or "N": ').lower()
+
+bill = 0
+if size == "s":
+  bill += 15
+elif size == "m":
+  bill += 20
+else:
+  bill += 25
+
+if add_pepperoni == "y":
+  if size == "s":
+    bill += 2
+  else:
+    bill += 3
+
+if extra_cheese == "y":
+  bill += 1
+
+print("Thank you for choosing Python Pizza Deliveries!")
+print(f"Your final bill is: ${bill}.")
